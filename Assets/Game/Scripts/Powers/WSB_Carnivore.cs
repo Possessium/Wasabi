67b2fe47e241f5f0bb183dc514c328ec7eff56fb
HugoG <guerquin.hugo@gmail.com>
2021-05-08 15:39:53 +0200

    Remove coroutine on carnivore

--- Assets/Game/Scripts/Powers/WSB_Carnivore.cs
+++ Assets/Game/Scripts/Powers/WSB_Carnivore.cs
@@ -8,49 +8,25 @@ public class WSB_Carnivore : WSB_Power
     [SerializeField] LayerMask eatLayer = 0;
     bool isEating = false;
 
-    public override void Start()
+    public override void Update()
     {
-        base.Start();
+        base.Update();
 
-        // Start the main coroutine
-        StartCoroutine(Check());
-    }
+        if (!IsActive || isEating)
+            return;
 
-    IEnumerator Check()
-    {
-        // Will loop until the object is disabled or destroyed
-        while(true)
+        Collider2D[] _hits = Physics2D.OverlapCircleAll(transform.position, range, eatLayer);
+        // If found any, eat them
+        if (_hits.Length > 0)
         {
-            while(!IsActive || !IsActive)
-            {
-                yield return new WaitForSeconds(.5f);
-            }
-
-            // Buffer if the plant is eating something
-            if (isEating)
-            {
-                yield return new WaitForEndOfFrame();
-            }
-
-            else
-            {
-                // Search for element around range to eat
-                Collider2D[] _hits = Physics2D.OverlapCircleAll(transform.position, range, eatLayer);
-                // If found any, eat them
-                if (_hits.Length > 0)
-                    StartCoroutine(Eat(_hits[0]));
-                else
-                    yield return new WaitForSeconds(2);
-            }
+            Destroy(_hits[0]);
+            StartCoroutine(Eat());
         }
     }
 
-    IEnumerator Eat(Collider2D _hit)
+    IEnumerator Eat()
     {
         isEating = true;
-
-        // Destroy next item
-        Destroy(_hit.gameObject);
 
         // Wait for given delay
         yield return new WaitForSeconds(eatDelay);
