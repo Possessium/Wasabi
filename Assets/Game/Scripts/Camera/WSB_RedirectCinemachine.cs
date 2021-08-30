fcf859102022e9572ab4236c95532020f61bbc67
HugoG <hugo.guerquin@outlook.fr>
2021-08-30 15:13:07 +0200

    ampoules plus grosses et bonne activation sur elevator (et grass qui passe cette fois)

--- Assets/Game/Scripts/Camera/WSB_RedirectCinemachine.cs
+++ Assets/Game/Scripts/Camera/WSB_RedirectCinemachine.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class WSB_RedirectCinemachine : MonoBehaviour
 {
     [SerializeField] WSB_TriggerCam triggerCam = null;
-
+    [SerializeField] WSB_LightBulb bulb = null;
 
     public void AnimationEnded() => triggerCam.AnimationEnded();
+
+    public void DisableBulb() => bulb.DeactivateBulb();
 }
