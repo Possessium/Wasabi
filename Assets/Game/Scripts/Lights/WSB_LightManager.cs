37bd55d835bdbe5d4f22b7c81430f3947cabf292
Hogu <hugo.guerquin@outlook.fr>
2021-09-24 10:34:58 +0200

    Light refait avec Yoni

--- Assets/Game/Scripts/Lights/WSB_LightManager.cs
+++ Assets/Game/Scripts/Lights/WSB_LightManager.cs
@@ -18,31 +18,31 @@ public class WSB_LightManager : MonoBehaviour
 
     private void Awake() => I = this;
 
-    public void ChangeColor(Color _c, bool _lux)
+    public void ChangeColor(Color _c, float _intensity, bool _lux)
     {
         if(_lux)
         {
             if (luxCoroutine != null)
                 StopCoroutine(luxCoroutine);
 
-            luxCoroutine = StartCoroutine(ChangeColor(true, _c));
+            luxCoroutine = StartCoroutine(ChangeColor(true, _intensity, _c));
         }
         else
         {
             if (banCoroutine != null)
                 StopCoroutine(banCoroutine);
 
-            banCoroutine = StartCoroutine(ChangeColor(false, _c));
+            banCoroutine = StartCoroutine(ChangeColor(false, _intensity, _c));
         }
     }
 
-    IEnumerator ChangeColor(bool _lux, Color _c)
+    IEnumerator ChangeColor(bool _lux, float _intensity, Color _c)
     {
         Color _tempColor = Color.white;
         Color _tempColorBack = Color.white;
         if (_lux)
         {
-            while(luxLight.color != _c || luxLightBack.color != _c)
+            while(luxLight.color != _c || luxLightBack.color != _c || luxLight.intensity != _intensity || luxLightBack.intensity != _intensity)
             {
                 _tempColor = luxLight.color;
                 _tempColorBack = luxLightBack.color;
@@ -56,12 +56,14 @@ public class WSB_LightManager : MonoBehaviour
                     Mathf.MoveTowards(_tempColorBack.g, _c.g, Time.deltaTime * moveColorSpeed),
                     Mathf.MoveTowards(_tempColorBack.b, _c.b, Time.deltaTime * moveColorSpeed),
                     Mathf.MoveTowards(_tempColorBack.a, _c.a, Time.deltaTime * moveColorSpeed));
+                luxLight.intensity = Mathf.MoveTowards(luxLight.intensity, _intensity, Time.deltaTime * moveColorSpeed);
+                luxLightBack.intensity = Mathf.MoveTowards(luxLightBack.intensity, _intensity, Time.deltaTime * moveColorSpeed);
                 yield return new WaitForEndOfFrame();
             }
         }
         else
         {
-            while(banLight.color != _c)
+            while(banLight.color != _c || banLightBack.color != _c || banLight.intensity != _intensity || banLightBack.intensity != _intensity)
             {
                 _tempColor = banLight.color;
                 _tempColorBack = banLightBack.color;
@@ -75,6 +77,8 @@ public class WSB_LightManager : MonoBehaviour
                     Mathf.MoveTowards(_tempColorBack.g, _c.g, Time.deltaTime * moveColorSpeed),
                     Mathf.MoveTowards(_tempColorBack.b, _c.b, Time.deltaTime * moveColorSpeed),
                     Mathf.MoveTowards(_tempColorBack.a, _c.a, Time.deltaTime * moveColorSpeed));
+                banLight.intensity = Mathf.MoveTowards(banLight.intensity, _intensity, Time.deltaTime * moveColorSpeed);
+                banLightBack.intensity = Mathf.MoveTowards(banLightBack.intensity, _intensity, Time.deltaTime * moveColorSpeed);
                 yield return new WaitForEndOfFrame();
             }
         }
--- Assets/Game/Scripts/Lights/WSB_LightSwitcher.cs
+++ Assets/Game/Scripts/Lights/WSB_LightSwitcher.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class WSB_LightSwitcher : MonoBehaviour
 {
     [SerializeField] private Color targetColor = Color.white;
+    [SerializeField] private float targetIntensity = 2;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<WSB_Lux>())
         {
-            WSB_LightManager.I.ChangeColor(targetColor, true);
+            WSB_LightManager.I.ChangeColor(targetColor, targetIntensity, true);
         }
 
         if (collision.GetComponent<WSB_Ban>())
         {
-            WSB_LightManager.I.ChangeColor(targetColor, false);
+            WSB_LightManager.I.ChangeColor(targetColor, targetIntensity, false);
         }
     }
 }
